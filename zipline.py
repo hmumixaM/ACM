c8c3ea665b07a6bbb39b759c85e2ebcaf48fb538
hmumixaM <max@12450.top>
2019-10-09 00:16:59 -0400

    Zipline Solution

--- zipline.py
+++ zipline.py
@@ -4,6 +4,10 @@ num = int(input())
 
 for i in range(num):
     w, g, h, r = list(map(int, input().split()))
-    max = math.sqrt(w**2 + abs(g-h)**2)
-
-    print("{:.8f} {:.8f}".format(max, min))
+    min = math.sqrt(w**2 + abs(g-h)**2)
+    if 2 * r == g + h:
+        d = w / 2
+    else:
+        d = (g - r) * w / (g + h - 2 * r)
+    max = math.sqrt(d**2 + abs(g-r)**2) + math.sqrt((w-d)**2 + abs(h-r)**2)
+    print("{:.8f} {:.8f}".format(min, max))
