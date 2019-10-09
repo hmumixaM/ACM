6908140a596e09ee5ec45ff7bc469369a6d4b24b
hmumixaM <max@12450.top>
2019-10-08 23:54:42 -0400

    Zipline Update

--- zipline.py
+++ zipline.py
@@ -4,5 +4,6 @@ num = int(input())
 
 for i in range(num):
     w, g, h, r = list(map(int, input().split()))
-    max = math.sqrt(w**2 + abs(g-h))
-    min = math.sqrt(w**2 + abs(g-h)) + math.sqrt(w**2 + abs(g-h))
+    max = math.sqrt(w**2 + abs(g-h)**2)
+
+    print("{:.8f} {:.8f}".format(max, min))
